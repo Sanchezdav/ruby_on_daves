fea4657ef2671425a533fb8c43cfaac2e89388ab
David Sanchez <sanchez.dav90@gmail.com>
2020-10-01 15:45:57 -0500

    Initial commit

---
+++ lib/ruby_on_daves.rb
@@ -0,0 +1,6 @@
+require "ruby_on_daves/version"
+
+module RubyOnDaves
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/ruby_on_daves/version.rb
@@ -0,0 +1,3 @@
+module RubyOnDaves
+  VERSION = "0.1.0"
+end
