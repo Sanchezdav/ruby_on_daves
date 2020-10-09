e506e4b44f5a7f7c775a49a919bea479c9bcbd1a
David Sanchez <sanchez.dav90@gmail.com>
2020-10-09 14:43:39 -0500

    Add initial routing

--- lib/ruby_on_daves.rb
+++ lib/ruby_on_daves.rb
@@ -1,11 +1,28 @@
 require "ruby_on_daves/version"
+require "ruby_on_daves/routing"
 
 module RubyOnDaves
   class Application
     def call(env)
-      `echo debug > debug.txt`;
+      if env['PATH_INFO'] == '/favicon.ico'
+        return [404, {'Content-Type' => 'text/html'}, []]
+      end
+
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
+      text = controller.send(act)
       [200, {'Content-Type' => 'text/html'},
-        ["Hello from Ruby on Daves!"]]
+        [text]]
+    end
+  end
+
+  class Controller
+    def initialize(env)
+      @env = env
+    end
+
+    def env
+      @env
     end
   end
 end
